f895590e56c49c2f97d2b28e97f1a077673552a9
Chris Myers <cmyers78@gmail.com>
2017-04-12 22:29:05 -0400

    Created BaseViewController Template App

---
+++ iOS-Template/CodeDoesGood-iOSTemplate/CodeDoesGood-iOSTemplate/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  CodeDoesGood-iOSTemplate
+//
+//  Created by Christopher Myers on 4/12/17.
+//  Copyright © 2017 Code Does Good. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
---
+++ iOS-Template/CodeDoesGood-iOSTemplate/CodeDoesGood-iOSTemplateTests/CodeDoesGood_iOSTemplateTests.swift
@@ -0,0 +1,36 @@
+//
+//  CodeDoesGood_iOSTemplateTests.swift
+//  CodeDoesGood-iOSTemplateTests
+//
+//  Created by Christopher Myers on 4/12/17.
+//  Copyright © 2017 Code Does Good. All rights reserved.
+//
+
+import XCTest
+@testable import CodeDoesGood_iOSTemplate
+
+class CodeDoesGood_iOSTemplateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
